b5969e349e6071f9786fa9ee591eaf9d167b2301
hb_dasheng <hb_dasheng@gmail.com>
2025-05-21 10:52:08 +0800

    feat: init project

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'vant/lab/index.css'
+import {Button} from 'vant'
+
+const app=createApp(App)
+
+app.use(Button)
+app.mount('#app')
